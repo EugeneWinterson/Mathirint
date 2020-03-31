27607577c2e01d955866d7e3c3dc4d5645eaa25d
EugeneWinterson <evgenyfurasev@gmail.com>
2020-03-31 18:04:58 +0300

    get vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,22 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    typeOfGame: 'none'
   },
   mutations: {
+    SET_TYPE_OF_GAME: (state, payload) => {
+      state.typeOfGame = payload;
+    }
   },
   actions: {
+    SET_TYPE_OF_GAME: (injectee, typeOfGame) => {
+      injectee.commit('SET_TYPE_OF_GAME', typeOfGame);
+    }
+  },
+  getters: {
+    typeGame: state => {
+      return state.typeOfGame;
+    }
   },
   modules: {
   }
